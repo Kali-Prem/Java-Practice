9c141c72562789006bd3f7fb07356317c18d97b3
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-01-21 20:55:08 +0530

    Create Print1To10.java

---
+++ 5. Loops/DoWhileLoopExample/1. Beginner level/Print1To10.java
@@ -0,0 +1,16 @@
+/*
+ Question:
+ Write a Java program to print numbers from 1 to 10
+ using a do-while loop.
+*/
+
+
+public class Print1To10 {
+    public static void main(String[] args) {
+        int i = 1;
+        do { 
+            System.out.println(i);
+            i++;
+        }while(i<=10);
+    }
+}
