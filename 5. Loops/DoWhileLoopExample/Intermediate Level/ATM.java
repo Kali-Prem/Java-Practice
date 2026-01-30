dbadbd4b816ccc2b46cf89054212fa24c07ddfbe
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-01-30 13:37:20 +0530

    Create ATM.java

---
+++ 5. Loops/DoWhileLoopExample/Intermediate Level/ATM.java
@@ -0,0 +1,70 @@
+import java.util.*;
+public class ATM {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        int Balance = 500;
+        int amount = 0;
+        int pin = 1234;
+        int auth;
+
+        do { 
+            System.out.println("=======ATM Menu=======");
+            System.out.println("1. Balance inquiry");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+
+            System.out.println("Enter Choice: ");
+            choice = sc.nextInt();
+
+            if(choice == 1){
+                System.out.println("Enter Your Pin: ");
+                auth = sc.nextInt();
+                if(auth == pin){
+                    System.out.println("Total Balance: "+ Balance);
+                }
+                else{
+                    System.out.println("Please enter Correct pin");
+                }
+                break;
+                   
+            }
+            else if(choice == 2){
+                System.out.println("Enter Your Pin: ");
+                auth = sc.nextInt();
+                if(auth == pin){
+                    System.out.println("Enter Amount:");
+                    amount = sc.nextInt();
+                    Balance = Balance + amount;
+
+                }
+                else{
+                    System.out.println("Please Enter correct pin");
+                }
+                break;
+                
+            }
+            else if(choice == 3){
+                System.out.println("Enter Your Pin: ");
+                auth = sc.nextInt();
+                if(auth == pin){
+                    System.out.print("Enter Widhrawal amount: ");
+                    amount = sc.nextInt();
+                    Balance = Balance - amount;
+                    System.out.println("Withdraw is Successfull");
+                    System.out.println("Remaining Balance: "+ Balance);
+
+                }
+                else{
+                    System.out.println("Please Enter correct pin");
+                }
+                break;
+                
+            }
+
+
+        } while (choice < 4);
+        System.out.println("Please Enter Valid Choice");
+    }
+}
