de76d5d429c42ed71d43e276b7e39d9578f832d6
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-01-30 13:50:54 +0530

    Update ATM.java

--- 5. Loops/DoWhileLoopExample/Intermediate Level/ATM.java
+++ 5. Loops/DoWhileLoopExample/Intermediate Level/ATM.java
@@ -4,7 +4,7 @@ public class ATM {
         Scanner sc = new Scanner(System.in);
         int choice;
         int Balance = 500;
-        int amount = 0;
+        int amount;
         int pin = 1234;
         int auth;
 
@@ -18,49 +18,45 @@ public class ATM {
             System.out.println("Enter Choice: ");
             choice = sc.nextInt();
 
-            if(choice == 1){
-                System.out.println("Enter Your Pin: ");
-                auth = sc.nextInt();
-                if(auth == pin){
-                    System.out.println("Total Balance: "+ Balance);
+            switch(choice){
+                case 1 -> {
+                    System.out.println("Enter Your Pin: ");
+                    auth = sc.nextInt();
+                    if(auth == pin){
+                        System.out.println("Total Balance: "+ Balance);
+                    }
+                    else{
+                        System.out.println("Please enter Correct pin");
+                    }
                 }
-                else{
-                    System.out.println("Please enter Correct pin");
+                    
+                case 2 -> {
+                    System.out.println("Enter Your Pin: ");
+                    auth = sc.nextInt();
+                    if(auth == pin){
+                        System.out.println("Enter Amount:");
+                        amount = sc.nextInt();
+                        Balance = Balance + amount;
+                    }
+                    else{
+                        System.out.println("Please Enter correct pin");
+                    }
                 }
-                break;
-                   
-            }
-            else if(choice == 2){
-                System.out.println("Enter Your Pin: ");
-                auth = sc.nextInt();
-                if(auth == pin){
-                    System.out.println("Enter Amount:");
-                    amount = sc.nextInt();
-                    Balance = Balance + amount;
-
+                    
+                case 3 -> {
+                    System.out.println("Enter Your Pin: ");
+                    auth = sc.nextInt();
+                    if(auth == pin){
+                        System.out.print("Enter Widhrawal amount: ");
+                        amount = sc.nextInt();
+                        Balance = Balance - amount;
+                        System.out.println("Withdraw is Successfull");
+                        System.out.println("Remaining Balance: "+ Balance);
+                    }
+                    else{
+                        System.out.println("Please Enter correct pin");
+                    }
                 }
-                else{
-                    System.out.println("Please Enter correct pin");
-                }
-                break;
-                
-            }
-            else if(choice == 3){
-                System.out.println("Enter Your Pin: ");
-                auth = sc.nextInt();
-                if(auth == pin){
-                    System.out.print("Enter Widhrawal amount: ");
-                    amount = sc.nextInt();
-                    Balance = Balance - amount;
-                    System.out.println("Withdraw is Successfull");
-                    System.out.println("Remaining Balance: "+ Balance);
-
-                }
-                else{
-                    System.out.println("Please Enter correct pin");
-                }
-                break;
-                
             }
 
 
