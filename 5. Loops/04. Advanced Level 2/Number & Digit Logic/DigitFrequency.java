bde456cbc5c0dfe751db6f9c954cf9c75260ad70
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-01-31 22:05:29 +0530

    Create DigitFrequency.java

---
+++ 5. Loops/04. Advanced Level 2/Number & Digit Logic/DigitFrequency.java
@@ -0,0 +1,13 @@
+/*
+ Question:
+ Write a Java program to find frequency of
+ each digit in a number.
+*/
+
+public class DigitFrequency {
+    public static void main(String[] args) {
+        int n = 1122233;
+        
+
+    }
+}
