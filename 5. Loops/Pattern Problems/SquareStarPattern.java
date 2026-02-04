eca6ce69477c9378dadd0d2dc735409c659c87cc
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-02-04 23:34:03 +0530

    Update SquareStarPattern.java

--- 5. Loops/Pattern Problems/SquareStarPattern.java
+++ 5. Loops/Pattern Problems/SquareStarPattern.java
@@ -10,6 +10,20 @@
 */
 
 public class SquareStarPattern {
+    public static void main(String[] args){
+        int n = 4;
+        for(int i = 1; i<= n; i++){
+            for(int j = 1; j<=n; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+/*
+public class SquareStarPattern {
     public static void main(String[] args) {
         
         for(int i = 1; i <= 4; i++) { // Outer loop for rows
@@ -21,7 +35,7 @@ public class SquareStarPattern {
         }
         
     }
-}
+}*/
 
 // Another Approach
 
