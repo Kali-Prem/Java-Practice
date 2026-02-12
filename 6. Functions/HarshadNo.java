f2c2c4011c5f2a03390af9c469caebb533164044
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-02-12 22:07:34 +0530

    Create HarshadNo.java

---
+++ 6. Functions/HarshadNo.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class HarshadNo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(checkHarshadNo(n) == true){
+            System.out.println("Number is harsed");
+        }
+        else{
+            System.out.print("-1");
+        }
+    }
+    public static boolean checkHarshadNo(int n){
+        int copy =  n;
+        int sum = 0;
+        while(copy > 0){
+            int digit = copy % 10;
+            sum = sum + digit;
+            copy = copy / 10;
+
+
+        }
+        if(copy % sum == 0) return true;
+        else return false;
+    }
+}
