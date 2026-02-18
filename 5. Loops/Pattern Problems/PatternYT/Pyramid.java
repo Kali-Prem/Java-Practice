f80e7da99ac2f93c98efe295b0f9972231ce8bb9
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-02-18 10:59:45 +0530

    Update Pyramid.java

--- 5. Loops/Pattern Problems/PatternYT/Pyramid.java
+++ 5. Loops/Pattern Problems/PatternYT/Pyramid.java
@@ -41,6 +41,26 @@ class Pyramid {
 
 */
 
+
+import java.util.*;
+class Pyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n-i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 1; j <= 2*i-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
+//===OR========
+
 /*
 import java.util.*;
 class Pyramid {
@@ -75,7 +95,7 @@ class Pyramid {
 
 */
 
-
+/*
 import java.util.*;
 class Pyramid {
     public static void main(String[] args) {
@@ -96,3 +116,4 @@ class Pyramid {
     }
 }
 
+*/
