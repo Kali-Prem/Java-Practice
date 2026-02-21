5a748d67fa940a08dcd5b07a2f86b9c2bbbe865c
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-02-22 03:40:39 +0530

    Create PrintHello.java

---
+++ 6. Functions/1. Level_01 Problems/PrintHello.java
@@ -0,0 +1,9 @@
+class PrintHello {
+    public static void main(String[] args){
+        printHello();
+
+    }
+    public static void printHello(){
+        System.out.print("Hello Java");
+    }
+}
