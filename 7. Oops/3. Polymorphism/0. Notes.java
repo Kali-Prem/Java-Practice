736db831d2f590a8b06fd32a61533f825e2b6fdd
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-02-25 00:07:33 +0530

    Update 0. Notes.java

--- 7. Oops/3. Polymorphism/0. Notes.java
+++ 7. Oops/3. Polymorphism/0. Notes.java
@@ -13,10 +13,9 @@ Error like: 1.) Overloading
 
 //===============1.) Overloading =====================
 /*
+# Note:- No errors in overloading because of different parameters but can increase complexity of code
 
-#Note:- 
-
-#Overlaoding:-> Function / Method Overloading
+# Overlaoding:-> Function / Method Overloading
 
 Function Name      -> Same
 Function Parameter -> Different
@@ -29,6 +28,24 @@ public static int add(int a, int b, int c){
     return a+b+c;
 }
 
+*/
 
+//=================2.) Overriding =========================
+/*
+# Overriding:-> Function / Method Overriding
+Function Name      -> Same
+Function Parameter -> Same
+
+# Note:- Compile time error in overriding because of same method name and same parameters but can be resolved by using @Override annotation
+#Solution:-> Use @Override annotation to resolve error Or make Overloading
+
+#Code : ->
+public static int add(int a, int b){
+    return a +b ;
+}
+public static int add(int a, int b){
+    return a+b;
+}
 
 */
+
