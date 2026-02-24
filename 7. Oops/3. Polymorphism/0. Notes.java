52102cdac397454d49ee099af88a1686e84f545d
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-02-24 23:51:26 +0530

    Update 0. Notes.java

---
+++ 7. Oops/3. Polymorphism/0. Notes.java
@@ -0,0 +1,34 @@
+//===========================PolyMorphism=========================
+/*
+Polymorphism : - Naam ek aur kaam anek
+
+
+Polymorphism generally Resolve Error
+
+Error like: 1.) Overloading
+            2.) Overriding
+
+
+*/
+
+//===============1.) Overloading =====================
+/*
+
+#Note:- 
+
+#Overlaoding:-> Function / Method Overloading
+
+Function Name      -> Same
+Function Parameter -> Different
+
+#Code : ->
+public static int add(int a, int b){
+    return a +b ;
+}
+public static int add(int a, int b, int c){
+    return a+b+c;
+}
+
+
+
+*/
