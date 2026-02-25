16344ef4234996b275f0522a240db12395780346
Prem Kumar <205642704+Kali-Prem@users.noreply.github.com>
2026-02-25 11:53:00 +0530

    Update main.java

--- 7. Oops/3. Polymorphism/main.java
+++ 7. Oops/3. Polymorphism/main.java
@@ -6,11 +6,22 @@ class main {
         System.out.println(add(a, b));
         System.out.println(add(a, b, c));
     }
-    //=======Overloading=================
+    //=======================Overloading=========================
     public static int add(int a, int b){
         return a+b;
     }
     public static int add(int a, int b, int c){
         return a+b+c;
     }
+
+    //=========================Overriding=========================
+    
+    //# This will give compile time error because of same method name and same parameters but can be resolved by using @Override annotation or make overloading
+    
+    // public static int add(int a, int b){ 
+    //     return a+b;
+    // }    
+    // public static int add(int a, int b){
+    //     return a+b;  
+    // }
 }
